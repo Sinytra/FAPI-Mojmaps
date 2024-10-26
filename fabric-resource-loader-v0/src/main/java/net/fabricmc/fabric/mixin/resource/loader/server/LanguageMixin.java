571e8ea1b68c462eb671cd6eb81c9ae87f3684cf
apple502j <33279053+apple502j@users.noreply.github.com>
2024-10-26 23:54:53 +0900

    Fix overriding vanilla translations (#4187)

--- fabric-resource-loader-v0/src/main/java/net/fabricmc/fabric/mixin/resource/loader/server/LanguageMixin.java
+++ fabric-resource-loader-v0/src/main/java/net/fabricmc/fabric/mixin/resource/loader/server/LanguageMixin.java
@@ -32,6 +32,8 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import net.fabricmc.fabric.impl.resource.loader.ServerLanguageUtil;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.locale.Language;
 
 @Mixin(Language.class)
@@ -47,6 +49,18 @@ class LanguageMixin {
 		}
 
 		return ImmutableMap.copyOf(map);
+	}
+
+	@Redirect(method = "parseTranslations(Ljava/util/function/BiConsumer;Ljava/lang/String;)V", at = @At(value = "INVOKE", target = "Ljava/lang/Class;getResourceAsStream(Ljava/lang/String;)Ljava/io/InputStream;"))
+	private static InputStream readCorrectVanillaResource(Class instance, String path) throws IOException {
+		ModContainer mod = FabricLoader.getInstance().getModContainer("minecraft").orElseThrow();
+		Path langPath = mod.findPath(path).orElse(null);
+
+		if (langPath == null) {
+			throw new IOException("Could not read %s from minecraft ModContainer".formatted(path));
+		} else {
+			return Files.newInputStream(langPath);
+		}
 	}
 
 	private static void loadFromPath(Path path, BiConsumer<String, String> entryConsumer) {

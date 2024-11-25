bf26711bf892b4b93a007cea3b0501fdf35a8ba8
modmuss <modmuss50@gmail.com>
2024-11-25 18:12:28 +0000

    Fix crash with custom signs. (#4240)

---
+++ fabric-object-builder-api-v1/src/client/java/net/fabricmc/fabric/mixin/object/builder/client/WoodTypeMixin.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.mixin.object.builder.client;
+
+import net.minecraft.client.renderer.Sheets;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.state.properties.WoodType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(WoodType.class)
+public abstract class WoodTypeMixin {
+	@Inject(method = "register", at = @At("RETURN"))
+	private static void register(WoodType type, CallbackInfoReturnable<WoodType> cir) {
+		final ResourceLocation identifier = ResourceLocation.parse(type.name());
+		Sheets.SIGN_MATERIALS.put(type, Sheets.createSignMaterial(identifier));
+		Sheets.HANGING_SIGN_MATERIALS.put(type, Sheets.createHangingSignMaterial(identifier));
+	}
+}
--- fabric-object-builder-api-v1/src/testmod/java/net/fabricmc/fabric/test/object/builder/TealSignTest.java
+++ fabric-object-builder-api-v1/src/testmod/java/net/fabricmc/fabric/test/object/builder/TealSignTest.java
@@ -66,6 +66,8 @@ public class TealSignTest implements ModInitializer {
 		Registry.register(BuiltInRegistries.ITEM, TEAL_HANGING_SIGN_KEY.location(), TEAL_HANGING_SIGN_ITEM);
 
 		BlockEntityType.SIGN.addSupportedBlock(TEAL_SIGN);
+		BlockEntityType.SIGN.addSupportedBlock(TEAL_WALL_SIGN);
 		BlockEntityType.HANGING_SIGN.addSupportedBlock(TEAL_HANGING_SIGN);
+		BlockEntityType.HANGING_SIGN.addSupportedBlock(TEAL_WALL_HANGING_SIGN);
 	}
 }

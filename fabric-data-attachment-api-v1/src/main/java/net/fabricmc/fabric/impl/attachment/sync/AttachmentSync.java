2345d86007e4d3eef7ecde13969ef9d96038f158
DennisOchulor <113277083+DennisOchulor@users.noreply.github.com>
2025-05-31 21:24:05 +0800

    Backport world attachment sync fix from #4366 (#4665)

--- fabric-data-attachment-api-v1/src/main/java/net/fabricmc/fabric/impl/attachment/sync/AttachmentSync.java
+++ fabric-data-attachment-api-v1/src/main/java/net/fabricmc/fabric/impl/attachment/sync/AttachmentSync.java
@@ -22,6 +22,7 @@ import java.util.Set;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.ServerEntityWorldChangeEvents;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerConfigurationConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerConfigurationNetworking;
@@ -105,6 +106,17 @@ public class AttachmentSync implements ModInitializer {
 			((AttachmentTargetImpl) player.serverLevel()).fabric_computeInitialSyncChanges(player, changes::add);
 			// sync player's own persistent attachments that couldn't be synced earlier
 			((AttachmentTargetImpl) player).fabric_computeInitialSyncChanges(player, changes::add);
+
+			if (!changes.isEmpty()) {
+				AttachmentChange.partitionAndSendPackets(changes, player);
+			}
+		});
+
+		ServerEntityWorldChangeEvents.AFTER_PLAYER_CHANGE_WORLD.register((player, origin, destination) -> {
+			// sync new world's attachments
+			// no conflict with previous one because the client world is recreated every time
+			List<AttachmentChange> changes = new ArrayList<>();
+			((AttachmentTargetImpl) destination).fabric_computeInitialSyncChanges(player, changes::add);
 
 			if (!changes.isEmpty()) {
 				AttachmentChange.partitionAndSendPackets(changes, player);

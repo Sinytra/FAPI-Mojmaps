357cded47d4d26b2ade30142a50722d082655f29
Juuz <6596629+Juuxel@users.noreply.github.com>
2025-06-27 14:48:59 +0300

    [1.21.1] Fix FabricRecipeProvider.getRecipeIdentifier not applying to recipe advancements (#4724)
    * Fix FabricRecipeProvider.getRecipeIdentifier not applying to recipe advancements
    
    Backport of #4703.
    
    * Fix getRecipeIdentifier applying twice to advancement paths

--- fabric-data-generation-api-v1/src/main/java/net/fabricmc/fabric/api/datagen/v1/provider/FabricRecipeProvider.java
+++ fabric-data-generation-api-v1/src/main/java/net/fabricmc/fabric/api/datagen/v1/provider/FabricRecipeProvider.java
@@ -80,6 +80,11 @@ public abstract class FabricRecipeProvider extends RecipeProvider {
 			public Advancement.Builder advancement() {
 				return exporter.advancement();
 			}
+
+			@Override
+			public ResourceLocation getRecipeIdentifier(ResourceLocation recipeId) {
+				return exporter.getRecipeIdentifier(recipeId);
+			}
 		};
 	}
 
@@ -90,10 +95,8 @@ public abstract class FabricRecipeProvider extends RecipeProvider {
 		buildRecipes(new RecipeOutput() {
 			@Override
 			public void accept(ResourceLocation recipeId, Recipe<?> recipe, @Nullable AdvancementHolder advancement) {
-				ResourceLocation identifier = getRecipeIdentifier(recipeId);
-
-				if (!generatedRecipes.add(identifier)) {
-					throw new IllegalStateException("Duplicate recipe " + identifier);
+				if (!generatedRecipes.add(recipeId)) {
+					throw new IllegalStateException("Duplicate recipe " + recipeId);
 				}
 
 				RegistryOps<JsonElement> registryOps = wrapperLookup.createSerializationContext(JsonOps.INSTANCE);
@@ -101,12 +104,12 @@ public abstract class FabricRecipeProvider extends RecipeProvider {
 				ResourceCondition[] conditions = FabricDataGenHelper.consumeConditions(recipe);
 				FabricDataGenHelper.addConditions(recipeJson, conditions);
 
-				list.add(DataProvider.saveStable(writer, recipeJson, recipePathProvider.json(identifier)));
+				list.add(DataProvider.saveStable(writer, recipeJson, recipePathProvider.json(recipeId)));
 
 				if (advancement != null) {
 					JsonObject advancementJson = Advancement.CODEC.encodeStart(registryOps, advancement.value()).getOrThrow(IllegalStateException::new).getAsJsonObject();
 					FabricDataGenHelper.addConditions(advancementJson, conditions);
-					list.add(DataProvider.saveStable(writer, advancementJson, advancementPathProvider.json(getRecipeIdentifier(advancement.id()))));
+					list.add(DataProvider.saveStable(writer, advancementJson, advancementPathProvider.json(advancement.id())));
 				}
 			}
 
@@ -114,6 +117,11 @@ public abstract class FabricRecipeProvider extends RecipeProvider {
 			public Advancement.Builder advancement() {
 				//noinspection removal
 				return Advancement.Builder.recipeAdvancement().parent(RecipeBuilder.ROOT_RECIPE_ADVANCEMENT);
+			}
+
+			@Override
+			public ResourceLocation getRecipeIdentifier(ResourceLocation recipeId) {
+				return FabricRecipeProvider.this.getRecipeIdentifier(recipeId);
 			}
 		});
 		return CompletableFuture.allOf(list.toArray(CompletableFuture[]::new));
